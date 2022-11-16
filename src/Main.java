3521c16f67d67ad21fb328417b74d040c3a9f4f2
kymdennis3 <kymdennis3@gmail.com>
2022-11-16 07:30:02 +0300

    created a second object and displayed one of their attributes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human human1 = new Human("Rick", 60, 70.0);
+       //Human human1 = new Human("Rick", 60, 70.0);
+        Human human2 = new Human("Morty", 16, 50.0);
 
-        System.out.println(human1.age);
+        System.out.println(human2.name);
     }
 }
