28598c5d15340ce6e4a54724fddb24136bdf80cd
kymdennis3 <kymdennis3@gmail.com>
2022-11-16 07:18:03 +0300

    testing to display an attribute of human1 (using constructors) without initiating it with this. (The output is null)

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+
+    String name;  //define the name,age and weight outside the constructor
+    int age;
+    double weight;
+
+    Human(String name, int age, double weight){
+
+        name = name; //assign the values in the constructor to each of the variable
+        age = age;
+        weight = weight;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Human human1 = new Human("Rick", 60, 70.0);
+
+        System.out.println(human1.age);
+    }
+}
