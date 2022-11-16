e6ce1d4795f1dfd81ad3abdd8bd1c602655e1350
kymdennis3 <kymdennis3@gmail.com>
2022-11-16 07:36:36 +0300

    instantiated two methods and used it with both of our objects

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-       //Human human1 = new Human("Rick", 60, 70.0);
+       Human human1 = new Human("Rick", 60, 70.0);
         Human human2 = new Human("Morty", 16, 50.0);
 
-        System.out.println(human2.name);
+        //System.out.println(human2.name);
+
+        human2.eat();
+        human1.drink();
     }
 }
